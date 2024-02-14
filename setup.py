b91a33b5c9a03936e20967814409520f0bce9e97
divanov@easytech.co <Korolishut911>
2024-02-14 13:58:26 +0200

    delete one print line for conflict

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
     print(long_description)
-    print('i was here and made changes in a new branch)
 
 setuptools.setup(
     name="metabase-api",
